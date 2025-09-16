8a089b98e39ea96dba0807dd79e12dcc2f80e609
B2B Scaler <your-email@b2bscaler.com>
2025-09-16 14:55:23 +0000

    fixed quote on page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
 import { Testimonials } from '@/components/Testimonials'
-import { ROICalculator } from '@/components/ROICalculator
+import { ROICalculator } from '@/components/ROICalculator'
 
 export default function Home() {
   return (
